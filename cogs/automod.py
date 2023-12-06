d52007cb80b16d2aa82f144d6de71dca1facbe4f
yorii260 <bordbox4@gmail.com>
2023-12-06 17:28:31 -0300

    Added AutoDelete (BETA)

--- cogs/automod.py
+++ cogs/automod.py
@@ -13,7 +13,7 @@ from helpers import utils
 import discord
 import random 
 from pymongo.collection import Collection
-
+import asyncio 
 
 automod_config = {
     
@@ -50,7 +50,7 @@ class AutomodException(commands.CheckFailure):
     
     def __init__(self, message: str, *args):
         super().__init__(message, *args)
-        self.message = message
+        self.message = message 
         
         
 class AutomodDatabase:
@@ -80,7 +80,7 @@ class AutomodDatabase:
         else:
             
             if roles.id in ammr['automod_moderator_roles']:
-                raise AutomodException(f"O role `{roles.id}` has already in the database, skipped.")
+                raise AutomodException(f"O cargo {role.mention if role.mentionable else role.id} já está na database.")
             ammr['automod_moderator_roles'].append(roles.id)
             return self.data.update_one({"_id": self._id}, {"$set": {"automod_config":ammr}}, upsert=True)
     
@@ -94,7 +94,7 @@ class AutomodDatabase:
             for role in roles:
                 
                 if role.id in ammr['automod_ignored_roles']:
-                    raise AutomodException(f"O role {role.id} has already in the database, skipped.")
+                    raise AutomodException(f"O cargo {role.mention if role.mentionable else role.id} já está na database.")
 
                 ammr['automod_ignored_roles'].append(role.id)
                 
@@ -102,7 +102,7 @@ class AutomodDatabase:
         else:
             
             if roles.id in ammr['automod_ignored_roles']:
-                raise AutomodException(f"O role {roles.id} has already in the database, skipped.")
+                raise AutomodException(f"O cargo {roles.mention if roles.mentionable else roles.id} já está na database.")
             ammr['automod_ignored_roles'].append(roles.id)
             return self.data.update_one({"_id": self._id}, {"$set": {"automod_config":ammr}}, upsert=True)
     
@@ -114,6 +114,35 @@ class AutomodDatabase:
         
         return self.data.update_one({"_id": self._id}, {"$set":{"automod_config":d}})
 
+    
+    def update_auto_ban(self, update):
+        
+        d: dict = self.dict[0]['automod_config']
+        d['auto_ban_config'].update(update)
+        
+        return self.data.update_one({"_id": self._id}, {"$set":{"automod_config":d}})
+    
+    
+    def update_auto_purge(self, update):
+        
+        d: dict = self.dict[0]['automod_config']
+        d['auto_purge_config'].update(update)
+        
+        return self.data.update_one({"_id": self._id}, {"$set":{"automod_config":d}})
+
+    
+    def update_kick_new_account(self, update):
+        
+        d: dict = self.dict[0]['automod_config']
+        d['kick_new_account_config'].update(update)
+        
+        return self.data.update_one({"_id": self._id}, {"$set":{"automod_config":d}})
+    
+
+    @property
+    def auto_delete_config(self):
+        return [y for f, y in self.dict[0]['automod_config']['auto_delete_config'].items()]
+    
 class AutomodCog(commands.Cog, name="Automod"):
     
     def __init__(self, bot: commands.Bot):
@@ -124,29 +153,39 @@ class AutomodCog(commands.Cog, name="Automod"):
         self.automod_ = AutomodDatabase(self.bot, self.bot.mongo.automod)
     
     
-    @commands.group(name='automod',
-                    description='Comandos relacionados ao AutoMod.')
-    async def automod(self, ctx: commands.Context):
-        pass 
+    @commands.Cog.listener('on_message')
+    async def automod_auto_delete(self, message: discord.Message):
+        
+        config = self.automod_.auto_delete_config
+        
+        # /////////////////////////////////////////////// # 
+        
+        ad_id = config[0]
+        filter = config[1]
+        punish = config[2]
+        channel = int(config[3])
+        
+
+        if (
+            not message.author.bot and 
+            message.channel.id == channel
+        ):
+            if filter != 'default':
+                
+                filter=filter.strip().split(':')
+                
+                if filter[0] == "SW":
+                    filter = bool(message.content.startswith(filter[1].lower()))
+                elif filter[0] == "EW":
+                    filter = bool(message.content.endswith(filter[1].lower()))
+            
+            else:
+                filter = True 
+
+            if filter:
+                
+                return await message.delete()
     
-    @automod.command(name='add_moderator_role',
-                     aliases=('amr',))
-    async def _(self, ctx: commands.Context, *, role: discord.Role):
-        self.automod_.update_moderator_roles(role)
-        return await ctx.send(f"`{role.name}` foi adicionado com sucesso.")
-    
-    
-    @automod.command(name='add_ad_filter',
-                     aliases=('adf',))
-    async def _(self, ctx: commands.Context, *, filter: str):
-        d = {
-            "auto_delete_id": random.randint(0, 99999),
-            "auto_delete_channel_id": ctx.channel.id,
-            "auto_delete_filter": filter,
-            "auto_delete_punish": "Ban"
-        }
-        self.automod_.update_auto_delete(d)
-        return await ctx.send("Update sucessfull.")
 
 
 async def setup(bot: commands.Bot):
